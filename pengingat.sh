fd88f5c73165e6ad2d5d8823b343354e19c635e2
MrVirusSpm-07 <81134812+MrVirusSpm-07@users.noreply.github.com>
2021-04-13 12:46:15 +0700

    Add files via upload

---
+++ pengingat.sh
@@ -0,0 +1,97 @@
+#tool deface pemula
+#tool pengingat
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+sleep 1
+echo
+echo $red" ____  _____ __  __ _   _ _        _     "
+echo $red"|  _ \| ____|  \/  | | | | |      / \    "
+echo $red"| |_) |  _| | |\/| | | | | |     / _ \   "
+echo $white"|  __/| |___| |  | | |_| | |___ / ___ \  "
+echo $white"|_|   |_____|_|  |_|\___/|_____/_/   \_\ "$blue"DEFACER-PENGINGAT"
+echo $yellow"##############################################################"
+sleep 2
+echo
+echo $yellow"۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞"
+echo $red"       (+) Author   : MR•VIRUS-SPM      (+)"
+echo $white"       (+) CNL YT   : MR VIRUS SPM NEW  (+)"
+echo $blue"       (+) TEAM     : WHITE CYBER UNION (+)"
+echo $yellow"۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞۞"
+echo
+sleep 2
+echo $white"1."$red" ( XSS FAKE   )"
+echo $white"2."$red" ( KINDEDITOR )"
+echo $white"3."$red" ( KELUAR     )"
+echo
+sleep 1
+read -p "pilih => " dor
+
+if [ $dor = 1 ]
+then
+clear
+sleep 1
+toilet -f big -F gay MR•VIRUS
+sleep 1
+echo $white
+echo "#BAHAN"
+echo "siapkan script deface"
+echo "cari website terserah"
+echo "satu lagi siapkan exploit"
+echo
+echo "untuk script deface berbentuk html terus anda ganti menjadi txt"
+echo
+echo "*CONTOH"
+echo "mrvirus.html menjadi mrvirus.txt"
+echo
+echo "lalu anda cari website mana saja terserah anda tapi ini hanyalah deface fake yh geng cuma bisa di screenshot"
+echo
+echo "setelah memilih website lalu anda ketik di di kolom pencarian dengan memasukan exploit ( javascript:prompt() )"
+echo
+echo "nah setelah itu tinggal anda masukan saja script deface anda yg sudah di ganti txt salin lalu pastekan"
+echo
+echo "lalu anda enter setelah itu website berhasil di deface 🤣"
+echo
+echo "KALO GK PAHAM BISA LIAT CENEL MR VIRUS SPM NEW"
+echo "================================================="
+echo
+fi
+if [ $dor = 2 ]
+then
+clear
+sleep 2
+echo
+echo "DEFACE METODE KINDEDITOR"
+echo "==========================="
+echo
+echo "PERTAMA kita cari dork inurl:examples/uploadbutton.html"
+echo
+echo "pilih salah satu website yg mau anda deface"
+echo
+echo "lalu anda klik upload"
+echo
+echo "anda bisa upload script deface anda"
+echo
+echo "setelah itu anda salin yg sudah berhasil di upload"
+echo
+echo "paste dan sambungkan nama web dengan script yg berhasil di upload"
+echo
+echo "CONTOH"
+echo "nama website : http://www.diandingding.com/Public/Admin/KindEditor/examples/uploadbutton.html"
+echo "yg harus di ganti: http://www.dianding.com/pastekan script yg udh di upload"
+echo
+echo "lalu enter dan website berhasil anda deface"
+echo
+sleep 2
+fi
+
+if [ $dor = 3 ]
+then
+exit
+fi
