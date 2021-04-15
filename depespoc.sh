64e67fde85a32f6dd5e1a8cc7f5eff3859be962f
MrVirusSpm-07 <81134812+MrVirusSpm-07@users.noreply.github.com>
2021-04-15 09:42:26 +0700

    Add files via upload

---
+++ depespoc.sh
@@ -0,0 +1,66 @@
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+clear
+echo
+sleep 1
+echo $green"×××××××××××××××××××××××××××××××××××××××××××××××××××××××××"
+echo $purple" (+) AUTHOR       : MR VIRUS SPM          (+)"
+echo $purple" (+) YOUTUBE      : MR VIRUS SPM NEW      (+)"
+echo $purple" (+) TEAM         : WHITE CYBER UNION     (+)"
+echo $green"×××××××××××××××××××××××××××××××××××××××××××××××××××××××××"
+echo $white
+echo "BERIKUT ADALAH WEBSITE YG BISA ANDA DEFACE DGN MUDAH"
+echo $green"======================================================="
+sleep 1
+echo
+echo $white"1.) " $red"REGU.MOE"
+echo $white"2.) " $red"KOSRED.COM"
+echo $white"3.) " $red"NEKO.AIRFORCE"
+echo $white"4.) " $red"DORK ASLINYA"
+echo $white"5.) " $red"KELUAR"
+echo $green
+sleep 1
+read -p "MASUKAN PILIHAN MU COK => " ger
+
+if [ $ger = 1 ]
+then
+clear
+sleep 1
+xdg-open https://regu.moe/
+fi
+
+if [ $ger = 2 ]
+then
+clear
+sleep 1
+xdg-open https://kosred.com/
+fi
+
+if [ $ger = 3 ]
+then
+clear
+sleep 1
+xdg-open https://img.neko.airforce/
+fi
+
+if [ $ger = 4 ]
+then
+clear
+echo
+echo
+echo
+echo "#DORK"
+echo
+echo $yellow"("$white" intext: select or drop file(s)"$ywllow" )"
+fi
+
+if [ $ger = 5 ]
+then
+   exit -y
+fi
